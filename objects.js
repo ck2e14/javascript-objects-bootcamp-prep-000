f2d848929fe251b77e5cb5d1e7075a6981bc4577
Christopher Kennedy <chris94kennedy@gmail.com>
2019-06-12 09:06:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  var newPlaylist = 
-  delete playlist.artistName
+  var newPlaylist = delete playlist.artistName
+ return newPlaylist
 }
